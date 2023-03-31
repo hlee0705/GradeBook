92bfd9b47da72566fc25737490235aca11270d75
Hyunkyoung Lee <HLee@1800contacts.com>
2023-03-31 15:10:48 -0600

    Add project files.

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var gradeList = new List<double>
+            {
+                averageGrade
+            };
+
+            var sumOfGrades = +averageGrade;
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            }
+
+ 
+            return 'F';
+        }
+    }
+}
